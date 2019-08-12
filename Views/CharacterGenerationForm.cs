d8e43f2e46ca983f6445376293d3205ab515147e
André Brandão Teodoro <andrebrandaoca@gmail.com>
2019-08-12 15:30:03 -0400

    Added Tab Control

--- Views/CharacterGenerationForm.cs
+++ Views/CharacterGenerationForm.cs
@@ -5,7 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+/*
+ * STUDENT NAME: ANDRE BRANDAO TEODORO
+ * STUDENT ID: 300944427
+ * DESCRIPTION: This is the main form for the application
+ */
 namespace COMP123_S2019_FinalTestC.Views
 {
     public partial class CharacterGenerationForm : COMP123_S2019_FinalTestC.Views.MasterForm
@@ -13,6 +17,30 @@ namespace COMP123_S2019_FinalTestC.Views
         public CharacterGenerationForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the event handler for the BackButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+        /// <summary>
+        /// This is the event handler for the NextButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedIndex < MainTabControl.TabPages.Count -1)
+            {
+                MainTabControl.SelectedIndex++;
+            }
         }
     }
 }
